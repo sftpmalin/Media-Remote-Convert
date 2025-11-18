6da4e617d6af9047f8bdb3b3f714505f3a53ec51
sftpmalin <sftpmalin@gmail.com>
2025-11-18 18:23:11 +0100

    Update and rename FFmpeg Putty.bat to Script Windows Remote/FFmpeg Putty.bat

--- Script Windows Remote/FFmpeg Putty.bat
+++ Script Windows Remote/FFmpeg Putty.bat
@@ -1,9 +1,10 @@
 @echo off
-set SERVER_IP=192.168.1.25
-set USER=yoan
-set KEY_PATH=D:\Nextcloud\keys\yoan.ppk
-set SESSION_NAME="debian"
+set SERVER_IP=192.168xxxxxxxxxxxxx
+set USER=user xxxxxxxxxxxxxxx
+set KEY_PATH=votre clexxxxx
+set SESSION_NAME="votre profile session"
 set PUTTY_PATH="C:\Program Files (x86)\Putty 0.79\Putty 0.79 x64\putty.exe"
 
 start "" %PUTTY_PATH% -load %SESSION_NAME% -ssh %USER%@%SERVER_IP% -i "%KEY_PATH%" -P 22
+
 
